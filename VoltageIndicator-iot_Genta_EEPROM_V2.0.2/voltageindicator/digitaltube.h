4d91fde4cbd34877a6d0dd29be671a696d3aa208
Notos_GentaLee <1822417391@qq.com>
2025-03-20 11:28:25 +0800

    add(EEPROM sendstr succeed)

---
+++ VoltageIndicator-iot_Genta_EEPROM_V2.0.2/voltageindicator/digitaltube.h
@@ -0,0 +1,17 @@
+
+static unsigned int case_cnt; 
+
+static unsigned int display_sram;
+
+void Set_AllPin_INPUT(void);
+
+void Display_Scan1(void);
+
+void Display_Scan2(void);
+
+void Display_Scan3(void);
+
+void Display_Scan4(void);
+
+extern void Display_tube(unsigned char);
+
